ebf64708396a7f51cfaeea4b1d79b88840fc7b6f
DaManflow <manonglaude7@gmail.com>
2023-06-09 16:46:29 +0200

    premier commit

---
+++ projet-eclipse/src/TestArdoise.java
@@ -0,0 +1,13 @@
+import ardoise.*;
+public class TestArdoise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Ardoise ardoise = new Ardoise();
+		
+		ardoise.test();
+
+	}
+
+}
