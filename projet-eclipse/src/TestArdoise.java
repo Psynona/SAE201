2d40ffb7a797b7a5be65befb682ab6e8f1a51d61
Manon GLAUDE <manon.glaude@edu.univ-paris13.fr>
2023-06-14 13:02:57 +0000

    Replace TestArdoise.java

--- projet-eclipse/src/TestArdoise.java
+++ projet-eclipse/src/TestArdoise.java
@@ -1,14 +1,36 @@
 import ardoise.*;
+import java.util.*;
+
 public class TestArdoise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Ardoise ardoise = new Ardoise();
-		System.out.println("COUCOU");
 		
 		ardoise.test();
+		
+		/*
+		 * Tester la classe Quadrilatere
+		 */
+		Quadrilatere q1 = new Quadrilatere();
+		PointPlan p1 = new PointPlan(10,10);
+		PointPlan p2 = new PointPlan(40,50);
+		Forme q2 = new Quadrilatere("Rectangle",p1,p2);
+		
+		ardoise.ajouterForme(q2);
+		
 
+		/*
+		 * Tester la classe Triangle
+		 */
+		
+		
+		/*
+		 * Tester la classe Chapeau
+		 */
+		
+		
 	}
 
 }
