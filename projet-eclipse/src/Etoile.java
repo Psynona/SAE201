fdb7b8c411750ac8219042b8eb7742389a3da681
DaManflow <manonglaude7@gmail.com>
2023-06-14 22:06:52 +0200

    ajout des versions testées

--- projet-eclipse/src/Etoile.java
+++ projet-eclipse/src/Etoile.java
@@ -1,25 +1,30 @@
 import java.util.*;
 import ardoise.*;
 public class Etoile extends Forme {
-	
+    
     private ArrayList<Chapeau> chapeaux;
 
-    public Etoile(){} 
+    public Etoile(){}
 
     public Etoile(Etoile uneEtoile) {
-    	super(uneEtoile.getNomForme());
+        super(uneEtoile.getNomForme());
         this.chapeaux = new ArrayList<Chapeau>();
         this.chapeaux = uneEtoile.getChapeaux();
         
     }
 
     public Etoile(Chapeau chap1, Chapeau chap2, Chapeau chap3, Chapeau chap4, String unNom) {
-    	super(unNom);
-    	this.chapeaux = new ArrayList<Chapeau>();
-        this.chapeaux.add(chap1);
-        this.chapeaux.add(chap2);
-        this.chapeaux.add(chap3);
-        this.chapeaux.add(chap4);
+        try{
+            super(unNom);
+            this.chapeaux = new ArrayList<Chapeau>();
+            this.chapeaux.add(chap1);
+            this.chapeaux.add(chap2);
+            this.chapeaux.add(chap3);
+            this.chapeaux.add(chap4);
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres" + e);
+        }
         
     }
 
@@ -28,22 +33,43 @@ public class Etoile extends Forme {
     }
 
     public Chapeau getChapeau(int i){
-        return this.chapeaux.get(i);
+        try{
+            return this.chapeaux.get(i);
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres"+ e);
+        }
+        catch(IndexOutOfBoundsException e) {
+            System.out.println("Vous avez saisi un indice trop élevé"+ e);
+        }
     }
 
     public void setChapeau(int i, Chapeau unChapeau){
-        this.chapeaux.set(i, unChapeau);
+        try{
+            this.chapeaux.set(i, unChapeau);
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres"+ e);
+        }
+        catch(IndexOutOfBoundsException e) {
+            System.out.println("Vous avez saisi un indice trop élevé"+ e);
+        }
     } 
 
     public String typeForme() {
         return "GF";
     }
-    
+
     public void deplacer(int deplacementX, int deplacementY) {
-        this.getChapeau(0).deplacer(deplacementX, deplacementY);
-        this.getChapeau(1).deplacer(deplacementX, deplacementY);
-        this.getChapeau(2).deplacer(deplacementX, deplacementY);
-        this.getChapeau(3).deplacer(deplacementX, deplacementY);
+        try{
+            this.getChapeau(0).deplacer(deplacementX, deplacementY);
+            this.getChapeau(1).deplacer(deplacementX, deplacementY);
+            this.getChapeau(2).deplacer(deplacementX, deplacementY);
+            this.getChapeau(3).deplacer(deplacementX, deplacementY);
+        }
+        catch (NullPointerException e) {
+            System.out.println("Il n'y a pas assez de segments"+ e);
+        }
     }
 
     public String toString() {
@@ -53,16 +79,21 @@ public class Etoile extends Forme {
     
     
      public ArrayList<Segment> dessiner(){
-     	ArrayList<Segment> res = new ArrayList<Segment>();
-     	res.add(this.getChapeau(1).dessiner().get(0));
-     	res.add(this.getChapeau(1).dessiner().get(1));
-     	res.add(this.getChapeau(0).dessiner().get(0));
-     	res.add(this.getChapeau(0).dessiner().get(1));
-     	res.add(this.getChapeau(2).dessiner().get(0));
-     	res.add(this.getChapeau(2).dessiner().get(1));
-     	res.add(this.getChapeau(3).dessiner().get(0));
-     	res.add(this.getChapeau(3).dessiner().get(1));
-		return res;
+        try{
+            ArrayList<Segment> res = new ArrayList<Segment>();
+            res.add(this.getChapeau(1).dessiner().get(0));
+            res.add(this.getChapeau(1).dessiner().get(1));
+            res.add(this.getChapeau(0).dessiner().get(0));
+            res.add(this.getChapeau(0).dessiner().get(1));
+            res.add(this.getChapeau(2).dessiner().get(0));
+            res.add(this.getChapeau(2).dessiner().get(1));
+            res.add(this.getChapeau(3).dessiner().get(0));
+            res.add(this.getChapeau(3).dessiner().get(1));
+            return res;
+        }
+        catch (NullPointerException e) {
+            System.out.println("Il n'y a pas assez de segments"+ e);
+        }
      
      }
 }
--- projet-eclipse/src/Maison.java
+++ projet-eclipse/src/Maison.java
@@ -2,8 +2,8 @@ import java.util.*;
 import ardoise.*;
 
 public class Maison extends Forme {
-	
-	//variables 
+    
+    //variables 
     private Quadrilatere corps;
     private Quadrilatere porte;
     private Chapeau toit;
@@ -38,15 +38,30 @@ public class Maison extends Forme {
     }
 
     public void setCorps(Quadrilatere unCorps) {
-        this.corps = unCorps;
+        try{
+            this.corps = unCorps;
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres"+ e);
+        }
     }
 
     public void setPorte(Quadrilatere unePorte) {
-        this.porte = unePorte;
+        try{
+            this.porte = unePorte;
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres"+ e);
+        }
     }
 
     public void setToit(Chapeau unToit) {
-        this.toit = unToit;
+        try{
+            this.toit = unToit;
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres"+ e);
+        }
     }
 
     public String typeForme() {
@@ -54,9 +69,14 @@ public class Maison extends Forme {
     }
 
     public void deplacer(int deplacementX, int deplacementY) {
-        this.corps.deplacer(deplacementX, deplacementY);
-        this.porte.deplacer(deplacementX, deplacementY);
-        this.toit.deplacer(deplacementX, deplacementY);
+        try{
+            this.corps.deplacer(deplacementX, deplacementY);
+            this.porte.deplacer(deplacementX, deplacementY);
+            this.toit.deplacer(deplacementX, deplacementY);
+        }
+        catch(IllegalStateException e) {
+            System.out.println("Vous avez oublié un ou plusieurs paramètres" + e);
+        }
     }
     
     public String toString() {
@@ -64,21 +84,26 @@ public class Maison extends Forme {
     }
 
     public ArrayList<Segment> dessiner(){
-        ArrayList<Segment> res = new ArrayList<Segment>();
-         
-         res.add(this.getPorte().dessiner().get(0));
-         res.add(this.getPorte().dessiner().get(1));
-         res.add(this.getPorte().dessiner().get(2));
-         res.add(this.getPorte().dessiner().get(3));
+        try{
+            ArrayList<Segment> res = new ArrayList<Segment>();
+            
+            res.add(this.getPorte().dessiner().get(0));
+            res.add(this.getPorte().dessiner().get(1));
+            res.add(this.getPorte().dessiner().get(2));
+            res.add(this.getPorte().dessiner().get(3));
 
-         res.add(this.getCorps().dessiner().get(0));
-         res.add(this.getCorps().dessiner().get(1));
-         res.add(this.getCorps().dessiner().get(2));
-         res.add(this.getCorps().dessiner().get(3));
-         
-         res.add(this.getToit().dessiner().get(0));
-         res.add(this.getToit().dessiner().get(1));
-        return res;
+            res.add(this.getCorps().dessiner().get(0));
+            res.add(this.getCorps().dessiner().get(1));
+            res.add(this.getCorps().dessiner().get(2));
+            res.add(this.getCorps().dessiner().get(3));
+            
+            res.add(this.getToit().dessiner().get(0));
+            res.add(this.getToit().dessiner().get(1));
+            return res;
+        }
+        catch (NullPointerException e) {
+            System.out.println("Il n'y a pas assez de segments"+ e);
+        }
 
     }
 } // fin classe Maison
